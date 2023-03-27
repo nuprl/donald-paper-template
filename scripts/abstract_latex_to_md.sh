81b21ed482effca598585ecde2a29ee2224bbaeb
Donald Pinckney <donald_pinckney@icloud.com>
2023-03-27 19:00:20 -0400

    Update abstract_latex_to_md.sh

--- scripts/abstract_latex_to_md.sh
+++ scripts/abstract_latex_to_md.sh
@@ -5,5 +5,5 @@ out_path="$2"
 
 pandoc -f latex -t json -o "$in_path.json" "$in_path"
 python3 scripts/strip_bad_pandoc_formats.py "$in_path.json" "$in_path.clean.json"
-pandoc -f json -t markdown -o "$out_path" "$in_path.clean.json"
+pandoc --wrap=none -f json -t markdown -o "$out_path" "$in_path.clean.json"
 rm "$in_path.json" "$in_path.clean.json"
